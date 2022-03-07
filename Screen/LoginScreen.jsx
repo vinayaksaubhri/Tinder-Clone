37500007a0c36dd718c919b4f4f860f9ff7c73a1
Vinayak Saubhri <vinayaksaubhri@Vinayaks-MacBook-Pro.local>
2022-03-07 17:13:01 +0530

    login in screen

--- Screen/LoginScreen.jsx
+++ Screen/LoginScreen.jsx
@@ -1,12 +1,48 @@
-import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import useAuth from "../Hooks/useAuth";
 
 const LoginScreen = () => {
   const { user, signInWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>{loading ? "Loading..." : "Login to the App"}</Text>
-      <Button title="Login" onPress={signInWithGoogle} />
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        resizeMode="cover"
+        style={{ flex: 1 }}
+        source={{ uri: "https://tinder.com/static/tinder.png" }}
+      >
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 160,
+            width: 208,
+            marginHorizontal: "25%",
+            backgroundColor: "white",
+            padding: 16,
+            borderRadius: 16,
+            fontWeight: "900",
+          }}
+        >
+          <Text style={{ textAlign: "center" }} onPress={signInWithGoogle}>
+            Sign in & Get Swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
