b11e7dc27aa388985f2e92da3b6ba9d8cf186c97
Vinayak Saubhri <vinayaksaubhri@Vinayaks-MacBook-Pro.local>
2022-03-08 14:54:40 +0530

    HomeScreen Header

--- Screen/HomeScreen.jsx
+++ Screen/HomeScreen.jsx
@@ -1,22 +1,52 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import useAuth from "../Hooks/useAuth";
+import GlobalStyle from "../Style/GlobalStyle";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const Navigation = useNavigation();
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Go to chat screen"
-        onPress={() => {
-          Navigation.navigate("Chat");
+    <SafeAreaView style={GlobalStyle.AndroidSafeArea}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingHorizontal: 20,
+          alignItems: "center",
         }}
-      />
-      <Button title="Logout" onPress={logout} />
-    </View>
+      >
+        <TouchableOpacity onPress={logout}>
+          <Image
+            style={{ height: 40, width: 40, borderRadius: 40 / 2 }}
+            source={{ uri: user.photoURL }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={{ height: 56, width: 56 }}
+            resizeMode="contain"
+            source={require("../Image/Tinder-Logo.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Navigation.navigate("Chat");
+          }}
+        >
+          <Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
